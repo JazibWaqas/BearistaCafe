e2908a0dd3a9826ad61e26970597e209e8dc222b
JazibWaqas <122615612+JazibWaqas@users.noreply.github.com>
2025-03-09 00:55:50 +0500

    images added and homepage done

--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import './styles/commonStyles.css'
+import './styles/home.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
