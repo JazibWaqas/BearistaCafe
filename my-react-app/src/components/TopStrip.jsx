8a93e3ad2555e9234a9a37e74c1b5cc599041b8f
SimraShamim <simrashamimss@gmail.com>
2025-03-19 22:13:26 +0500

    cart

--- my-react-app/src/components/TopStrip.jsx
+++ my-react-app/src/components/TopStrip.jsx
@@ -1,14 +1,5 @@
 import { Link } from 'react-router-dom';
 
 export default function TopStrip() {
-  return (
-    <div className="top-strip d-flex justify-content-end align-items-center p-2">
-      <Link to="/login" className="custom-link me-3" title="Login/Register">
-        <i className="fas fa-user"></i>
-      </Link>
-      <Link to="/cart" className="cart-link" title="Shopping Cart">
-        <i className="fas fa-shopping-cart"></i>
-      </Link>
-    </div>
-  );
+  return
 }
